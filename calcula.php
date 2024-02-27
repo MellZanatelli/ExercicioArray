8bcbd6c1d632e23cf236c83c1c7e600ff261e589
MellZanatelli <mell.zanatelli@gmail.com>
2024-02-27 17:39:31 -0300

    27/02

---
+++ calcula.php
@@ -0,0 +1,32 @@
+<?php
+    $nome = $_POST['nome'];
+    $idade = $_POST['idade'];
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <title>Resultado</title>
+</head>
+<body>
+<div class="container py-4">
+            <div class="jumbotron">
+                <p class="h3 text-center">Terceirão 2024</p>
+            </div>
+        <div class="row py-4">
+            <div class="col-sm-12 text-center">
+                <p><?php echo "Olá " . $nome . "! Você tem " . $intervalo->format( '%Y anos' ) . "!";?></p>
+            </div>
+        </div>
+    </div>
+    <div class="container py-4">
+        <div class="card-footer text-center">
+            <p>&copy;CEFET-MG Vargiha 2024</p>
+        </div>
+    </div>
+</body>
+</html>
